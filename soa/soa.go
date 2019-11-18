fc1f6a7b2ee1a2d7d29a2d30a9a55c071ef859fd
lt <1029563229@qq.com>
2019-11-18 17:03:46 +0800

    修改路由选择配置

--- soa/soa.go
+++ soa/soa.go
@@ -1,8 +1,10 @@
 package soa
 
 import (
+	"encoding/json"
 	"fmt"
-	"lib/soa/sutils"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -26,56 +28,104 @@ func (ctx *Ctx) init() {
 }
 
 func (ctx *Ctx) End(status int, message string) {
+	ctx.w.Header().Set("content-type", "application/json")
 	ctx.w.WriteHeader(status)
 	fmt.Fprintf(ctx.w, message)
+}
+
+func (ctx *Ctx) JSON(jsonData interface{}) string {
+	jsonBytes, err := json.Marshal(&jsonData) //转换成 JSON，返回的是 byte[]
+	if err != nil {
+		log.Println(err)
+		return "Parse JSON Has Error"
+	}
+
+	return string(jsonBytes)
+}
+
+func (ctx *Ctx) Query(key string) string {
+	return ctx.r.URL.Query().Get(key)
+}
+
+func (ctx *Ctx) QueryInt(key string) int64 {
+	val, err := strconv.ParseInt(ctx.r.URL.Query().Get(key), 10, 64)
+	if err != nil {
+		log.Println(err)
+		return 0
+	}
+	return val
+}
+
+func (ctx *Ctx) Body() map[string]interface{} {
+	bytes, _ := ioutil.ReadAll(ctx.r.Body)
+	body := make(map[string]interface{})
+	err := json.Unmarshal(bytes, &body)
+	if err != nil {
+		log.Println(err)
+	}
+	return body
+}
+
+func (ctx *Ctx) GetBody(receiver interface{}) interface{} {
+	bytes, _ := ioutil.ReadAll(ctx.r.Body)
+	err := json.Unmarshal(bytes, receiver)
+	if err != nil {
+		log.Println(err)
+	}
+	return receiver
 }
 
 type Handle func(ctx *Ctx)
 
 type Middleware func(Handle) Handle
 
-var routes = make(map[string][]string)
-
-func routeController() Middleware {
-	return func(next Handle) Handle {
-		return func(ctx *Ctx) {
-			if !sutils.Includes(routes[ctx.Request.URL], ctx.Request.Method) {
-				http.NotFound(ctx.w, ctx.r)
-				return
-			}
-			next(ctx)
-		}
-	}
-}
-
-func (s *Server) Listen(port int) {
-	p := strconv.Itoa(port)
-	fmt.Println("server is listening in http://localhost:" + p)
-	http.ListenAndServe(":"+p, nil)
-}
+var routes = make(map[string]Handle)
 
 func (s *Server) GET(uri string, handle Handle, middlewares ...Middleware) {
 	s.SetRequest("GET", uri, handle, middlewares...)
 }
 
+func (s *Server) PUT(uri string, handle Handle, middlewares ...Middleware) {
+	s.SetRequest("PUT", uri, handle, middlewares...)
+}
+
+func (s *Server) POST(uri string, handle Handle, middlewares ...Middleware) {
+	s.SetRequest("POST", uri, handle, middlewares...)
+}
+
+func (s *Server) DELETE(uri string, handle Handle, middlewares ...Middleware) {
+	s.SetRequest("DELETE", uri, handle, middlewares...)
+}
+
 func (s *Server) SetRequest(method string, uri string, handle Handle, middlewares ...Middleware) {
-	routes[uri] = append(routes[uri], method)
-	handle = Chain(handle, routeController())
-	handle = Chain(handle, middlewares...)
-	http.HandleFunc(uri, http.HandlerFunc(ctxInject(handle)))
+	handle = chain(handle, middlewares...)
+	routeId := method + uri
+	routes[routeId] = handle
 }
 
-func ctxInject(handle Handle) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := Ctx{w: w, r: r}
-		ctx.init()
-		handle(&ctx)
-	}
-}
-
-func Chain(h Handle, middlewares ...Middleware) Handle {
+func chain(h Handle, middlewares ...Middleware) Handle {
 	for _, m := range middlewares {
 		h = m(h)
 	}
 	return h
+}
+
+func (s *Server) Listen(port int) {
+	http.HandleFunc("/", buildRouter)
+	p := strconv.Itoa(port)
+	fmt.Println("server is listening in http://localhost:" + p)
+	http.ListenAndServe(":"+p, nil)
+}
+
+func buildRouter(w http.ResponseWriter, r *http.Request) {
+	routeId := r.Method + r.URL.Path
+	handle, ok := routes[routeId]
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+
+	ctx := Ctx{w: w, r: r}
+	ctx.init()
+	handle(&ctx)
 }
